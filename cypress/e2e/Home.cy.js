ddf1728b5d0ec076fe6722be5ae6d487391ecb35
AutomationMastery <automation.mastery.2026@gmail.com>
2026-02-12 18:02:38 +0530

    Create Home.cy.js

---
+++ cypress/e2e/Home.cy.js
@@ -0,0 +1,12 @@
+
+//import 'cypress-mochawesome-reporter/register';
+Cypress.on('uncaught:exception', (err, runnable) => {
+    return false
+})
+
+describe('CoreTechAutomation', () => {
+    it('Passed', () => {
+        cy.visit("http://coretechautomation.ct.ws/")
+        cy.url().should('include', 'http://coretechautomation.ct.ws/')
+    })
+})
